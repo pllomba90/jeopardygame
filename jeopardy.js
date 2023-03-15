95111ca9b7fe93133154c48b8bfbba91da1e7e64
Peter Lombardo <peterllombardo@gmail.com>
2023-03-15 17:56:21 -0400

    Intial code commit

---
+++ jeopardy.js
@@ -0,0 +1,112 @@
+// Jeopardy functionality plan. 
+//First I want a function to create the board. It should probably be a table to allow for
+// table headers as categories and consistent dollar amounts. It should be a 6x6 grid 
+// of 36 cards.
+//Second I will want a function to query the jeopardy API at "http://jservice.io/api/categories". I will need it to 
+//populate the categories and five clues for each category. I will use this function to also place clues and categories on the board.
+//Third I will want a function to start the game. This will be a simple event listener that will call the 
+// previous functions.
+// Fourth I will want a function to handle the clicks on the individual cards. I will need to have a single click
+//to display the clue and either a second click required to show the answer or set a timeout to display the answer.
+//Fifth I will want a function to end the game. This will need to recognize when all clues have been selected
+//and alert the player to the end of the game. 
+// Sixth and bonus only if I have the time I would like to have a start game page that transistions
+//into the game. Something like the tv screen image. 
+// categories is the main data structure for the app; it looks like this:
+
+//  [
+//    { title: "Math",
+//      clues: [
+//        {question: "2+2", answer: 4, showing: null},
+//        {question: "1+1", answer: 2, showing: null}
+//        ...
+//      ],
+//    },
+//    { title: "Literature",
+//      clues: [
+//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
+//        {question: "Bell Jar Author", answer: "Plath", showing: null},
+//        ...
+//      ],
+//    },
+//    ...
+//  ]
+
+let categories = [];
+
+
+/** Get NUM_CATEGORIES random category from API.
+ *
+ * Returns array of category ids
+ */
+
+function getCategoryIds() {
+}
+
+/** Return object with data about a category:
+ *
+ *  Returns { title: "Math", clues: clue-array }
+ *
+ * Where clue-array is:
+ *   [
+ *      {question: "Hamlet Author", answer: "Shakespeare", showing: null},
+ *      {question: "Bell Jar Author", answer: "Plath", showing: null},
+ *      ...
+ *   ]
+ */
+
+function getCategory(catId) {
+}
+
+/** Fill the HTML table#jeopardy with the categories & cells for questions.
+ *
+ * - The <thead> should be filled w/a <tr>, and a <td> for each category
+ * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
+ *   each with a question for each category in a <td>
+ *   (initally, just show a "?" where the question/answer would go.)
+ */
+
+async function fillTable() {
+}
+
+/** Handle clicking on a clue: show the question or answer.
+ *
+ * Uses .showing property on clue to determine what to show:
+ * - if currently null, show question & set .showing to "question"
+ * - if currently "question", show answer & set .showing to "answer"
+ * - if currently "answer", ignore click
+ * */
+
+function handleClick(evt) {
+}
+
+/** Wipe the current Jeopardy board, show the loading spinner,
+ * and update the button used to fetch data.
+ */
+
+function showLoadingView() {
+
+}
+
+/** Remove the loading spinner and update the button used to fetch data. */
+
+function hideLoadingView() {
+}
+
+/** Start game:
+ *
+ * - get random category Ids
+ * - get data for each category
+ * - create HTML table
+ * */
+
+async function setupAndStart() {
+}
+
+/** On click of start / restart button, set up game. */
+
+// TODO
+
+/** On page load, add event handler for clicking clues */
+
+// TODO
